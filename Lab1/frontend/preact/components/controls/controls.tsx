a18ebdce99d4560700fa1f066c11b7d634c08f9d
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-09-21 18:48:27 -0500

    tryin to style that button :/

--- Lab1/frontend/preact/components/controls/controls.tsx
+++ Lab1/frontend/preact/components/controls/controls.tsx
@@ -1,11 +1,17 @@
 import { h, Component } from 'preact';
-
+import { Button } from 'preact-fluid';
 import { auth, googleAuthProvider } from '../firebase/firebase';
+
+import style from './controls.scss';
 
 interface MyProps {}
 interface MyState {
     currentUser: any;
 }
+
+const theme = {
+    primaryColor: '#72D7C2'
+};
 
 export default class Controls extends Component<MyProps, MyState> {
     constructor() {
@@ -25,13 +31,25 @@ export default class Controls extends Component<MyProps, MyState> {
         });
     }
 
-    // <button onClick={() => auth.signInWithRedirect(googleAuthProvider)}>Sign In</button>
     // {user && <p>Signed In As: {name}</p>}
     // <button onClick={() => auth.signOut()}>Sign Out</button>
 
     render() {
         // const user = this.state.currentUser;
         // const name = user ? user.displayName : 'No one';
-        return <h2>Controls</h2>;
+        return (
+            <div class={style.Stage}>
+                <div class={style.Over}>
+                    <h3>Sign in to access remote sensor controls</h3>
+                    <Button class={style.Button} theme={theme} primary rounded onClick={() => auth.signInWithRedirect(googleAuthProvider)}>
+                        Sign In
+                    </Button>
+                </div>
+                <div class={style.Middle} />
+                <div class={style.Under}>
+                    <h2>Controls Hidden</h2>
+                </div>
+            </div>
+        );
     }
 }
