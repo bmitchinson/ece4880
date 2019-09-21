6409a39e4541fe541e6713d03e37eaff3ca9d9ef
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-09-21 16:29:59 -0500

    Removed unused mui

--- Lab1/frontend/preact/components/app.tsx
+++ Lab1/frontend/preact/components/app.tsx
@@ -1,5 +1,4 @@
 import { h, Component } from 'preact';
-import { Grid, Cell, Card } from 'preact-fluid';
 
 import Header from './header/header';
 import ComponentGrid from './componentGrid/componentGrid';
@@ -8,18 +7,6 @@ interface MyProps {}
 interface MyState {}
 
 export default class App extends Component<MyProps, MyState> {
-    /** Gets fired when the route changes.
-     *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-     *	@param {string} event.url	The newly routed URL
-     */
-    currentUrl;
-
-    handleRoute = e => {
-        this.currentUrl = e.url;
-    };
-    // <GridRow extraSmall="10" small="10" medium="10" large="10">
-    // </GridRow>
-
     render() {
         return (
             <div id="app">
