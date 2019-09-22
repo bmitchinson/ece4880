f3e3e93cdd6c70c435e0f3191cc2e51d38192825
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-09-22 11:55:27 -0500

    separate controls into guard and from

--- Lab1/frontend/preact/components/controls/controls.tsx
+++ Lab1/frontend/preact/components/controls/controls.tsx
@@ -1,17 +1,15 @@
 import { h, Component } from 'preact';
-import { Button } from 'preact-fluid';
-import { auth, googleAuthProvider } from '../firebase/firebase';
+import { auth } from '../firebase/firebase';
 
-import style from './controls.scss';
+import Guard from './guard/gaurd';
+import Form from './form/form';
+
+// import style from './controls.scss';
 
 interface MyProps {}
 interface MyState {
     currentUser: any;
 }
-
-const theme = {
-    primaryColor: '#ffffff'
-};
 
 export default class Controls extends Component<MyProps, MyState> {
     constructor() {
@@ -23,34 +21,13 @@ export default class Controls extends Component<MyProps, MyState> {
 
     componentDidMount() {
         auth.onAuthStateChanged(currentUser => {
-            if (currentUser) {
-                this.setState({ currentUser }, () => console.log('Logged In!'));
-            } else {
-                this.setState({ currentUser }, () => console.log('Logged Out!'));
-            }
+            this.setState({ currentUser });
+            // Could have a callback to this that actually verifies their auth status + effects UI
         });
     }
 
     render() {
         const user = this.state.currentUser;
-        return (
-            <div class={style.Stage}>
-                {!user && (
-                    <div class={style.Over}>
-                        <h3>Sign in to access remote sensor controls</h3>
-                        <Button class={style.Button} theme={theme} size={'large'} primary onClick={() => auth.signInWithRedirect(googleAuthProvider)}>
-                            Sign In
-                        </Button>
-                    </div>
-                )}
-                {!user && <div class={style.Middle} />}
-                {!user && (
-                    <div class={style.Under}>
-                        <h2>Controls Hidden</h2>
-                    </div>
-                )}
-                {user && <h2>Controls Unlocked</h2>}
-            </div>
-        );
+        return user ? <Form /> : <Guard />;
     }
 }
---
+++ Lab1/frontend/preact/components/controls/form/form.tsx
@@ -0,0 +1,5 @@
+import { h } from 'preact';
+
+const Form = () => <h2>Controls Unlocked</h2>;
+
+export default Form;
