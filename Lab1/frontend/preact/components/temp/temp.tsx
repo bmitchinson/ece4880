333b068897dce1977656a808c8f3f1f820d7a04b
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-09-23 19:53:11 -0500

    Updates UI based on switch / sensor status

--- Lab1/frontend/preact/components/temp/temp.tsx
+++ Lab1/frontend/preact/components/temp/temp.tsx
@@ -6,16 +6,23 @@ import style from './temp.scss';
 const tempRef = database.collection('temps');
 const tempObj = tempRef.orderBy('time', 'desc').limit(1);
 
+const sensorRef = database.collection('toggles').doc('sensor');
+const switchRef = database.collection('toggles').doc('switch');
+
 interface MyProps {}
 interface MyState {
     displayTemp: number;
+    sensorIsDisconnected: boolean;
+    switchIsOn: boolean;
 }
 
 export default class Temp extends Component<MyProps, MyState> {
     constructor() {
         super();
         this.state = {
-            displayTemp: null
+            displayTemp: null,
+            sensorIsDisconnected: false,
+            switchIsOn: true
         };
     }
 
@@ -26,14 +33,33 @@ export default class Temp extends Component<MyProps, MyState> {
                 this.setState({ displayTemp: temp });
             });
         });
+        sensorRef.onSnapshot(doc => {
+            const sensorIsDisconnected = doc.data().isDisconnected;
+            this.setState(prev => ({ ...prev, sensorIsDisconnected }));
+        });
+        switchRef.onSnapshot(doc => {
+            const switchIsOn = doc.data().isOn;
+            this.setState(prev => ({ ...prev, switchIsOn }));
+        });
     }
 
     render() {
+        let tempHeader = 'Current Temp:';
+        if (this.state.sensorIsDisconnected) {
+            tempHeader = 'Unplugged Sensor';
+        }
+        if (!this.state.switchIsOn) {
+            tempHeader = 'No Data Available (Switch Off)';
+        }
         const currentTemp = this.state.displayTemp;
-        const tempText = currentTemp ? currentTemp + '°C' : '--';
+        const tempText =
+            currentTemp && tempHeader === 'Current Temp:'
+                ? currentTemp + '°C'
+                : '--';
+        console.log(this.state, tempHeader, tempText);
         return (
             <div>
-                <header class={style.TempLabel}>Current Temp:</header>
+                <header class={style.TempLabel}>{tempHeader}</header>
                 <p class={style.Temp}>{tempText}</p>
             </div>
         );
