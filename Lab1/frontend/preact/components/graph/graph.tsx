49155f74fb21ecb5664a90d47cf989f05149cbaf
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-09-28 19:58:22 -0500

    graph sketch and shortened patch

--- Lab1/frontend/preact/components/graph/graph.tsx
+++ Lab1/frontend/preact/components/graph/graph.tsx
@@ -1,5 +1,44 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
+import { database } from '../firebase/firebase';
+import { TrendChart, TimestampArray } from 'preact-charts';
 
-const Graph = () => <h2>Graph</h2>;
+interface MyProps {}
+interface MyState {
+    dataArray: TimestampArray;
+}
 
-export default Graph;
+export default class Graph extends Component<MyProps, MyState> {
+    constructor() {
+        super();
+        const dataArray = [];
+        for (let i = 0; i < 5; i += 1) {
+            const now = new Date();
+            dataArray.push({
+                timestamp: now.setSeconds(now.getSeconds() - i),
+                temp: Math.random() * 60 - 10
+            });
+        }
+        this.state = {
+            dataArray
+        };
+    }
+
+    // componentDidMount() {}
+    // Fetch 300 seconds, reg collection onChange
+
+    // collection on change: grab 1 and add it to stack
+
+    render() {
+        const { dataArray } = this.state;
+        return (
+            <TrendChart
+                name="Temps"
+                x="timestamp"
+                y="temp"
+                data={dataArray}
+                margin={{ top: 60, right: 80, left: 80, bottom: 60 }}
+                axisControl={false}
+            />
+        );
+    }
+}
