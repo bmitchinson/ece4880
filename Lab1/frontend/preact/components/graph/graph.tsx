1c94cf603f10cd50d6af8f2f337ac73a06688724
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-09-30 00:34:20 -0500

    into functions, need to fix bounding to make move

--- Lab1/frontend/preact/components/graph/graph.tsx
+++ Lab1/frontend/preact/components/graph/graph.tsx
@@ -11,111 +11,16 @@ interface MyState {
 export default class Graph extends Component<MyProps, MyState> {
     constructor() {
         super();
-        const dataArray = [];
-        const dataCoverArray = [];
-        for (let i = 0; i < 33; i += 1) {
-            const time = new Date();
-            time.setSeconds(time.getSeconds() - i);
-            dataArray.push({
-                timestamp: time,
-                temp: Math.random() * 50
-            });
-        }
-        for (let i = 33; i < 66; i += 1) {
-            const time = new Date();
-            time.setSeconds(time.getSeconds() - i);
-            dataArray.push({
-                timestamp: time,
-                temp: -100
-            });
-        }
-        for (let i = 66; i < 99; i += 1) {
-            const time = new Date();
-            time.setSeconds(time.getSeconds() - i);
-            dataArray.push({
-                timestamp: time,
-                temp: Math.random() * 50
-            });
-        }
-        dataArray.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
-        dataCoverArray.push({
-            timestamp: dataArray[0].timestamp,
-            temp: 0
-        });
-        dataCoverArray.push({
-            timestamp: dataArray[98].timestamp,
-            temp: 0
-        });
-        // TODO: dataCoverLogic
-        dataArray.forEach((ele, index, dataArray) => {
-            if (
-                index !== 0 &&
-                ele.temp === -100 &&
-                dataArray[index - 1].temp !== -100
-            ) {
-                console.log('drop @', dataArray[index - 1].timestamp);
-                dataCoverArray.push({
-                    timestamp: dataArray[index - 1].timestamp.setMilliseconds(
-                        dataArray[index - 1].timestamp.getMilliseconds() - 50
-                    ),
-                    temp: 0
-                });
-                dataCoverArray.push({
-                    timestamp: dataArray[index - 1].timestamp,
-                    temp: dataArray[index - 1].temp
-                });
-                dataCoverArray.push({
-                    timestamp: ele.timestamp,
-                    temp: 10
-                });
-            } else if (
-                index !== dataArray.length - 1 &&
-                ele.temp === -100 &&
-                dataArray[index + 1].temp !== -100
-            ) {
-                console.log('rise @', dataArray[index + 1].timestamp);
-                dataCoverArray.push({
-                    timestamp: ele.timestamp,
-                    temp: 10
-                });
-                dataCoverArray.push({
-                    timestamp: dataArray[index + 1].timestamp,
-                    temp: dataArray[index + 1].temp
-                });
-                dataCoverArray.push({
-                    timestamp: dataArray[index + 1].timestamp.setMilliseconds(
-                        dataArray[index + 1].timestamp.getMilliseconds() + 50
-                    ),
-                    temp: 0
-                });
-            }
-        });
-        dataCoverArray.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
-        console.log('dataArray', dataArray, 'dataCoverArray', dataCoverArray);
-        dataArray.map(val => {
-            if (val.temp > 50) {
-                val.temp = 50;
-            }
-            if (val.temp < 0) {
-                val.temp = 10;
-            }
-            return val;
-        });
-        dataCoverArray.map(val => {
-            if (val.temp > 50) {
-                val.temp = 50;
-            }
-            if (val.temp < 0) {
-                val.temp = 10;
-            }
-            return val;
-        });
+        const dataArray = getFakeDataOne();
+        const dataCoverArray = getCoverArray(dataArray);
+        boundedDataArray(dataArray);
+        boundedDataArray(dataCoverArray);
 
         this.state = { dataArray, dataCoverArray };
     }
 
     componentDidMount() {
-        // setInterval(this.addNewPoint, 1000);
+        // setTimeout(this.addNewPoint, 3000);
     }
 
     addNewPoint = () => {
@@ -127,17 +32,24 @@ export default class Graph extends Component<MyProps, MyState> {
         });
         dataArrayEdit.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
         dataArrayEdit.splice(0, 1);
-        this.setState({ dataArray: dataArrayEdit });
-    };
-    // Fetch 300 seconds, reg collection onChange
+        const dataCoverArrayEdit = getCoverArray(dataArrayEdit);
 
-    // collection on change: grab 1 and add it to stack
+        this.setState({
+            dataArray: dataArrayEdit,
+            dataCoverArray: dataCoverArrayEdit
+        });
+    };
 
     render() {
         const { dataArray, dataCoverArray } = this.state;
-        console.log('given:', dataArray);
         dataArray[0].temp = 0;
         dataArray[1].temp = 50;
+        // console.log(
+        //     'data:',
+        //     boundedDataArray(dataArray),
+        //     'cover:',
+        //     boundedDataArray(dataCoverArray)
+        // );
         return (
             <TrendChart
                 lineColour="#90d7c2"
@@ -154,3 +66,112 @@ export default class Graph extends Component<MyProps, MyState> {
         );
     }
 }
+
+const boundedDataArray = array => {
+    array.map(val => {
+        if (val.temp > 50) {
+            val.temp = 50;
+        }
+        if (val.temp < 0) {
+            val.temp = 0;
+        }
+        return val;
+    });
+};
+
+// const boundedDataArray = array => {
+//     return array.map(val => {
+//         if (val.temp > 50) {
+//             return 50;
+//         }
+//         if (val.temp < 0) {
+//             return 0;
+//         }
+//         return val;
+//     });
+// };
+
+const getFakeDataOne = () => {
+    const dataArray = [];
+    for (let i = 0; i < 33; i += 1) {
+        const time = new Date();
+        time.setSeconds(time.getSeconds() - i);
+        dataArray.push({
+            timestamp: time,
+            temp: Math.random() * 50
+        });
+    }
+    for (let i = 33; i < 66; i += 1) {
+        const time = new Date();
+        time.setSeconds(time.getSeconds() - i);
+        dataArray.push({
+            timestamp: time,
+            temp: -100
+        });
+    }
+    for (let i = 66; i < 99; i += 1) {
+        const time = new Date();
+        time.setSeconds(time.getSeconds() - i);
+        dataArray.push({
+            timestamp: time,
+            temp: Math.random() * 50
+        });
+    }
+    dataArray.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
+    return dataArray;
+};
+
+const getCoverArray = dataArray => {
+    const dataCoverArray = [];
+    dataCoverArray.push({
+        timestamp: dataArray[0].timestamp,
+        temp: 0
+    });
+    dataCoverArray.push({
+        timestamp: dataArray[dataArray.length - 1].timestamp,
+        temp: 0
+    });
+    dataArray.forEach((ele, index, dataArray) => {
+        if (
+            index !== 0 &&
+            ele.temp === -100 &&
+            dataArray[index - 1].temp !== -100
+        ) {
+            dataCoverArray.push({
+                timestamp: dataArray[index - 1].timestamp.setMilliseconds(
+                    dataArray[index - 1].timestamp.getMilliseconds() - 2
+                ),
+                temp: 0
+            });
+            dataCoverArray.push({
+                timestamp: dataArray[index - 1].timestamp,
+                temp: dataArray[index - 1].temp
+            });
+            dataCoverArray.push({
+                timestamp: ele.timestamp,
+                temp: 0
+            });
+        } else if (
+            index !== dataArray.length - 1 &&
+            ele.temp === -100 &&
+            dataArray[index + 1].temp !== -100
+        ) {
+            dataCoverArray.push({
+                timestamp: ele.timestamp,
+                temp: 0
+            });
+            dataCoverArray.push({
+                timestamp: dataArray[index + 1].timestamp,
+                temp: dataArray[index + 1].temp
+            });
+            dataCoverArray.push({
+                timestamp: dataArray[index + 1].timestamp.setMilliseconds(
+                    dataArray[index + 1].timestamp.getMilliseconds() + 2
+                ),
+                temp: 0
+            });
+        }
+    });
+    dataCoverArray.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
+    return dataCoverArray;
+};
