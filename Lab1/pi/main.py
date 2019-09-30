8f9cae393ff0fac9be068773b7e6f7cd467317e6
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-09-30 18:43:26 -0500

    Merge in pi-setup

--- Lab1/pi/main.py
+++ Lab1/pi/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Alexander Powers (alexander-powers@uiowa.edu)
 
+# for delaying each individual process ( so they take turns better )
 from time import sleep
 
 # thermometer library
@@ -17,9 +18,33 @@ from Temperature import Temperature
 # LETS MULTI-PROCESS THIS BITCH
 from multiprocessing import Manager, Process
 
+# Display Libraries
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+
+# really image processing library, but used for LCD
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
 cred = credentials.Certificate("./lab1-firebase-admin-sdk-key.json")
 firebase_admin.initialize_app(cred)
-SLEEP_DURATION = 1
+sleep_duration = 1
+
+DC = 23
+RST = 24
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+font = ImageFont.load_default()
+
+# Initialize library.
+disp.begin(contrast=60)
+
+# Clear display.
+disp.clear()
+disp.display()
 
 
 def button_is_pressed():
@@ -32,15 +57,13 @@ def switch_is_on():
     return True
 
 
-def read_and_send_temp_process(state):
+def read_and_send_temp_process(state, individual_run=False):
     db = firestore.client()
     temps = db.collection(u"temps")
     sensor = W1ThermSensor()
     while True:
         sleep(SLEEP_DURATION)
-        send_temps = (
-            switch_is_on()
-        )  # && some logic from firestore get this variable from the current state
+        send_temps = switch_is_on() and state['a']
         if send_temps:
             new_doc = temps.document()
             current_temp = senesor.get_temperature
@@ -50,7 +73,25 @@ def read_and_send_temp_process(state):
             print("Saved: {0}, to firestore".format(str(new_temp)))
 
 
-def update_state_process(state):
+def update_state_process(state, individual_run=False):
+    def write_unpluged_to_lcd():
+          image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+          draw = ImageDraw.Draw(image)
+          draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
+          formatted_str = "! DISCONNECTED !"
+          draw.text((8,LCD.LCDHEIGHT/2), formatted_str, font=font)
+          disp.image(image)
+          disp.display()
+
+    def write_temp_to_lcd(temperature : int):
+          image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+          draw = ImageDraw.Draw(image)
+          draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
+          formatted_str = "Temp: %.2f." % temperature
+          draw.text((8,LCD.LCDHEIGHT/2), formatted_str, font=font)
+          disp.image(image)
+          disp.display()
+
     db = firestore.client()
     prefs = db.collection(u"preferences")
     while True:
@@ -59,12 +100,26 @@ def update_state_process(state):
         state["b"] = not state["b"]
 
 
-def update_display_process(state):
+def update_display_process(state, individual_run=False):
+    disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+    font = ImageFont.load_default()
+    disp.begin(contrast=60)
+    disp.clear()
+    disp.display()
+
     while True:
         sleep(SLEEP_DURATION)
+        # check some condition
+
         print("idk what to do here yet")
         current_temp = state["current_temp"]
         # display the current temperature
+
+def hardware_io_process(state, individual_run=False):
+    # lol we should figure this out....
+    while True:
+        print('hardware sucks')
+        sleep(SLEEP_DURATION)
 
 
 def main():
@@ -73,15 +128,15 @@ def main():
         # Read initial state from firestore
         db = firestore.client()
         prefs = db.collection(u"preferences")
-        state_dict["state"] = {}  # put state here
-        state_dict["b"] = True
-        state_dict["current_temp"] = 5
-
+        state_dict["firestore_state"] = {}  # put state here
+        state_dict["local_state"] = {}
+        state_dict["current_temp"] = 0.0;
         try:
             processes = [
                 Process(target=read_and_send_temp_process, args=(state_dict,)),
                 Process(target=update_state_process, args=(state_dict,)),
                 Process(target=update_display_process, args=(state_dict,)),
+                Process(target=hardware_io_process, args=(state_dict,)),
             ]
             [p.start() for p in processes]
             [p.join() for p in processes]
--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 # Date: 20190915
 
 # script:
+cd `dirname ${0}`
 echo "git secret hide" >> ./.git/hooks/pre-commit
 echo "git secret reveal" >> ./.git/hooks/post-merge
+chmod 700 ./.git/hooks/pre-commit ./.git/hooks/post-merge
+
