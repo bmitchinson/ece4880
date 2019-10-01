4b24e7ae05955628af6dcb0bb38d719e29d36dc1
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-10-01 03:07:03 -0500

    initial firebase pull

--- Lab1/frontend/preact/components/graph/graph.tsx
+++ Lab1/frontend/preact/components/graph/graph.tsx
@@ -5,6 +5,9 @@ import { CubeGrid } from 'styled-loaders';
 
 import style from './graph.scss';
 
+const tempRef = database.collection('temps');
+const tempObj = tempRef.orderBy('time', 'desc').limit(300);
+
 interface MyProps {}
 interface MyState {
     loading: boolean;
@@ -15,10 +18,20 @@ interface MyState {
 const LOWER_BOUND = 10;
 const UPPER_BOUND = 50;
 
+const tempQueue = [];
+
 export default class Graph extends Component<MyProps, MyState> {
     constructor() {
         super();
-        const dataArray = getFakeDataOne();
+        const dataArray = [];
+        for (let i = 300; i > 0; i -= 1) {
+            const time = new Date();
+            time.setSeconds(time.getSeconds() - i);
+            dataArray.push({
+                timestamp: time,
+                temp: -100
+            });
+        }
         const dataCoverArray = getCoverArray(dataArray);
         // Set loading state
 
@@ -26,9 +39,43 @@ export default class Graph extends Component<MyProps, MyState> {
     }
 
     componentDidMount() {
+        // Create 300 slots
+        // [0] oldest
+        const dataArray = this.state.dataArray;
+
+        const zeroMinAgo = Math.floor(
+            dataArray[299].timestamp.getTime() / 1000
+        );
+        const fiveMinAgo = Math.floor(dataArray[0].timestamp.getTime() / 1000);
+
         // Fetch last 300 from firebase
-        // Iterate through all 300, assigning to 300 solts, determined by 300
-        //   slots from current time
+        let howmany = 0;
+        tempObj
+            .get()
+            .then(snap => {
+                snap.forEach(doc => {
+                    howmany += 1;
+                    // Newest in [0]
+                    const seconds = doc.data().time.seconds;
+                    const temp = doc.data().temp;
+                    if (seconds >= fiveMinAgo && seconds <= zeroMinAgo) {
+                        const pos = 299 - (zeroMinAgo - seconds);
+                        dataArray[pos] = {
+                            timestamp: new Date(seconds * 1000),
+                            temp: temp.toFixed(1)
+                        };
+                    } else {
+                        console.log('data not in range:', doc.data());
+                    }
+                });
+                console.log('how many', howmany);
+                // Set an onChange that then populates a queue of incoming temps
+                // Every second, pull from that queue or create a dud if queue's empty
+                setInterval(this.addPointFromQueue, 1000);
+                this.setState({ loading: false });
+            })
+            .catch(e => console.log('Error getting documents:', e));
+
         // After set a function that every second:
         //   queries for the most recent one:
         //   if it's within a second of current time:
@@ -37,18 +84,19 @@ export default class Graph extends Component<MyProps, MyState> {
         //      add a -100 at the current time
         //   remove end.
         //
-        //setInterval(this.addNewPoint, 1000);
     }
 
-    addNewPoint = () => {
+    addPointFromQueue = () => {
         const dataArrayEdit = this.state.dataArray;
-        const time = new Date();
-        dataArrayEdit.push({
-            timestamp: time,
-            temp: Math.random() * 60 - 10
-        });
-        dataArrayEdit.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
         dataArrayEdit.splice(0, 1);
+
+        if (tempQueue.length) {
+        } else {
+            dataArrayEdit.push({
+                timestamp: new Date(),
+                temp: -100
+            });
+        }
         const dataCoverArrayEdit = getCoverArray(dataArrayEdit);
 
         this.setState({
@@ -57,22 +105,39 @@ export default class Graph extends Component<MyProps, MyState> {
         });
     };
 
+    addPointToQueue = () => {};
+
+    // addNewPointToDataArray = () => {
+    //     const dataArrayEdit = this.state.dataArray;
+    //     const time = new Date();
+    //     dataArrayEdit.push({
+    //         timestamp: time,
+    //         temp: Math.random() * 60 - 10
+    //     });
+    //     dataArrayEdit.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
+    //     dataArrayEdit.splice(0, 1);
+    //     const dataCoverArrayEdit = getCoverArray(dataArrayEdit);
+
+    //     this.setState({
+    //         dataArray: dataArrayEdit,
+    //         dataCoverArray: dataCoverArrayEdit
+    //     });
+    // };
+
     render() {
         const { dataArray, dataCoverArray, loading } = this.state;
-        dataArray[0].temp = LOWER_BOUND;
-        dataArray[1].temp = UPPER_BOUND;
 
         return !loading ? (
             <div class={style.ContainGraph}>
                 <div class={style.ContainChart}>
                     <TrendChart
                         lineColour="#90d7c2"
-                        lineColourTwo="#ffffff"
+                        //lineColourTwo="#ffffff"
                         name="Temps"
                         x="timestamp"
                         y="temp"
                         data={getBoundedDataArray(dataArray)}
-                        dataSetTwo={getBoundedDataArray(dataCoverArray)}
+                        //dataSetTwo={getBoundedDataArray(dataCoverArray)}
                         margin={{ top: 20, right: 20, left: 40, bottom: 10 }}
                         axisControl={false}
                         tooltip={false}
@@ -104,50 +169,52 @@ export default class Graph extends Component<MyProps, MyState> {
 }
 
 const getBoundedDataArray = array => {
-    return array.map(val => {
-        if (val.temp > UPPER_BOUND) {
-            return {
-                timestamp: new Date(val.timestamp),
-                temp: UPPER_BOUND
-            };
-        } else if (val.temp < LOWER_BOUND) {
-            return {
-                timestamp: new Date(val.timestamp),
-                temp: LOWER_BOUND
-            };
-        } else return val;
-    });
+    return array
+        .map(val => {
+            if (val.temp > UPPER_BOUND) {
+                return {
+                    timestamp: new Date(val.timestamp),
+                    temp: UPPER_BOUND
+                };
+            } else if (val.temp < LOWER_BOUND) {
+                return {
+                    timestamp: new Date(val.timestamp),
+                    temp: LOWER_BOUND
+                };
+            } else return val;
+        })
+        .sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
 };
 
-const getFakeDataOne = () => {
-    const dataArray = [];
-    for (let i = 0; i < 100; i += 1) {
-        const time = new Date();
-        time.setSeconds(time.getSeconds() - i);
-        dataArray.push({
-            timestamp: time,
-            temp: Math.random() * 60 - 10
-        });
-    }
-    for (let i = 100; i < 200; i += 1) {
-        const time = new Date();
-        time.setSeconds(time.getSeconds() - i);
-        dataArray.push({
-            timestamp: time,
-            temp: -100
-        });
-    }
-    for (let i = 200; i < 300; i += 1) {
-        const time = new Date();
-        time.setSeconds(time.getSeconds() - i);
-        dataArray.push({
-            timestamp: time,
-            temp: Math.random() * 60 - 10
-        });
-    }
-    dataArray.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
-    return dataArray;
-};
+// const getFakeDataOne = () => {
+//     const dataArray = [];
+//     for (let i = 0; i < 100; i += 1) {
+//         const time = new Date();
+//         time.setSeconds(time.getSeconds() - i);
+//         dataArray.push({
+//             timestamp: time,
+//             temp: Math.random() * 60 - 10
+//         });
+//     }
+//     for (let i = 100; i < 200; i += 1) {
+//         const time = new Date();
+//         time.setSeconds(time.getSeconds() - i);
+//         dataArray.push({
+//             timestamp: time,
+//             temp: -100
+//         });
+//     }
+//     for (let i = 200; i < 300; i += 1) {
+//         const time = new Date();
+//         time.setSeconds(time.getSeconds() - i);
+//         dataArray.push({
+//             timestamp: time,
+//             temp: Math.random() * 60 - 10
+//         });
+//     }
+//     dataArray.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
+//     return dataArray;
+// };
 
 const getCoverArray = dataArray => {
     const dataCoverArray = [];
@@ -207,6 +274,6 @@ const getCoverArray = dataArray => {
             });
         }
     });
-    dataCoverArray.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
+    // dataCoverArray.sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1));
     return dataCoverArray;
 };
