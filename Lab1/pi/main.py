2a5be1e3dfbdcae6262418eb3325fe02b61cf118
abpwrs <alexander-powers@uiowa.edu>
2019-10-02 17:58:14 -0500

    working pulling down state

--- Lab1/pi/main.py
+++ Lab1/pi/main.py
@@ -62,7 +62,7 @@ def read_temp_process(state):
     sensor = state['sensor']
     try:
         new_temp = Temperature(sensor.get_temperature())
-        state["current_temp"] = new_temp
+        state['current_temp'] = new_temp
         print("Saved: {0}".format(str(new_temp)))
         state['isDisconnected'] = False
     except Exception as e:
@@ -86,21 +86,21 @@ def send_connection_status_to_firebase(state):
 
 def send_temp_to_firebase(state):
     new_doc = state['temps_collection'].document()
-    new_temp = state["current_temp"]
+    new_temp = state['current_temp']
     new_doc.set(new_temp.to_dict(firestore_timestamp=True))
     print("Pushed: {0}".format(str(new_temp)))
 
 def send_text_if_required_process(state):
     try:
-        current_temp = state["current_temp"].temp 
+        current_temp = state['current_temp'].temp 
         under_low_threshold = current_temp < state["low_threshold"]
         over_high_threshold = current_temp > state["high_threshold"]
 
         if under_low_threshold or over_high_threshold:
             print('sending text')
             # TODO: Uncomment this if update_state_from_firebase_process works
-            msg = state["low_message"] if under_low_threshold else state["high_message"]
-            to = '1+' state["number_to_text"].replace('-','')
+            msg = state['low_msg'] if under_low_threshold else state["high_msg"]
+            to = '1+' + state['number_to_text'].replace('-','')
             # message = state['twilio_client'].messages.create(
             #     body='msg',
             #     from_='+12054984327',
@@ -122,15 +122,17 @@ def send_text_if_required_process(state):
 def update_state_from_firebase_process(state):
     try:
         prefs = state['prefs_document'].get().to_dict()
-        print('got', prefs)
-        state['low_message'] = prefs.low_msg
-        state['high_message'] = prefs.high_msg
-        state['number_to_text'] = prefs.phonenumber
-        state['low_threshold'] = prefs.low_temp
-        state['high_threshold'] = prefs.max_temp
+        print(prefs)
+        state['low_msg'] = prefs['low_msg']
+        state['high_msg'] = prefs['high_msg']
+        state['number_to_text'] = prefs['phonenumber']
+        state['low_threshold'] = prefs['low_temp']
+        state['high_threshold'] = prefs['max_temp']
+        # TODO: why 
+        # to = "1+" + state['number_to_text'].replace('-','')
 
-    except:
-        print('error getting prefs')
+    except Exception as e:
+        print('error getting prefs:', e)
 
 def update_display_process(state):
     if state['update_display'] and state['isPressed']:
@@ -159,7 +161,7 @@ def update_display_process(state):
               disp.display()
 
 
-        current_temp = state["current_temp"]
+        current_temp = state['current_temp']
         if state['isDisconnected']:
             write_unpluged_to_lcd()
         else:
@@ -167,7 +169,7 @@ def update_display_process(state):
 
 def main():
 
-    sleep(.5)
+    sleep(.1)
 
     # Pins
     GPIO.setmode(GPIO.BCM)
@@ -198,22 +200,22 @@ def main():
     state['isOn'] = not GPIO.input(SWITCH_PIN)
     state['update_display'] = False
     # Hardware Object Instances
-    state['sensor'] = sensor
+    # state['sensor'] = sensor
     state['font'] = font
     state['disp'] = disp
     # Temp
     state['current_temp'] = Temperature(0.0)
     # Firebase docs/collections
     state['temps_collection'] = db.collection(u"temps")
-    state['pref_document'] = db.collection(u"texting").document(u"prefs")
+    state['prefs_document'] = db.collection(u"texting").document(u"prefs")
     state['sensor_document'] = db.collection(u"toggles").document(u"sensor")
     state['switch_document'] = db.collection(u"toggles").document(u"switch")
     state['button_document'] = db.collection(u"toggles").document(u"button")
     # Twilio Object
     state['twilio_client'] = Client(TwilioCreds().sid, TwilioCreds().auth_token)
     # Local state from firebase
-    state['low_message'] = 'temp went too low'
-    state['high_message'] = 'temp '
+    state['low_msg'] = 'temp went too low'
+    state['high_msg'] = 'temp '
     state['number_to_text'] = '+16307404172'
     state['low_threshold'] = 10
     state['high_threshold'] = 30
@@ -262,7 +264,7 @@ def main():
         # schedule.every(0.01).seconds.do(run_threaded, job_function=update_display_process, state=state)
         # schedule.every(1).seconds.do(run_threaded, job_function=read_temp_process, state=state)
         # schedule.every(5).seconds.do(run_threaded, job_function=send_text_if_required_process, state=state)
-        schedule.every(1).seconds.do(run_threaded, job_function=update_state_from_firebase_process, state=state)
+        schedule.every(2).seconds.do(run_threaded, job_function=update_state_from_firebase_process, state=state)
 
     if state['isOn']:
         start_processes()
