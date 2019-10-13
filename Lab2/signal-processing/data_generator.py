48835088bab750fdcb4aef58f3bd061df290d43c
abpwrs <powersa15@gmail.com>
2019-10-13 09:38:17 -0500

    FIX: syntax error

--- Lab2/signal-processing/data_generator.py
+++ Lab2/signal-processing/data_generator.py
@@ -5,13 +5,13 @@ import numpy as np
 import time
 
 def generate_sin(magnitude=10, noise=True):
-    sin = np.sin(time.time()
+    sin = np.sin(time.time())
     if noise:
         sin += np.random.rand()
     return magnitude * sin
 
 def generate_cos(magnitude=10, noise=True):
-    cos = np.cos(time.time()
+    cos = np.cos(time.time())
     if noise:
         cos += np.random.rand()
     return magnitude * cos
