9a5f5a9e3337e8c50f60763cbf327f38f6835697
abpwrs <powersa15@gmail.com>
2019-10-13 10:08:24 -0500

    ENH: cleaning up data generator

--- Lab2/signal-processing/data_generator.py
+++ Lab2/signal-processing/data_generator.py
@@ -4,6 +4,7 @@
 import numpy as np
 import time
 
+
 def generate_sin(magnitude=10, noise=True, offset=0):
     sin = np.sin(time.time())
     if noise:
@@ -11,6 +12,7 @@ def generate_sin(magnitude=10, noise=True, offset=0):
     sin *= magnitude
     sin += offset
     return sin
+
 
 def generate_cos(magnitude=10, noise=True, offset=0):
     cos = np.cos(time.time())
@@ -20,30 +22,24 @@ def generate_cos(magnitude=10, noise=True, offset=0):
     cos += offset
     return cos
 
-def generate_jumpy_sin(magnitude=10, noise=True, time_count=0, jump_every=50, jump_offset=20):
+
+def generate_jumpy_sin(
+    magnitude=10, noise=True, time_count=0, jump_every=50, jump_offset=20
+):
     time_count = time_count // jump_every
-    if (time_count %  2 == 0):
+    if time_count % 2 == 0:
         offset = jump_offset
     else:
         offset = 0
-        
-    sin = np.sin(time.time())
-    if noise:
-        sin += np.random.rand()
-    sin *= magnitude
-    sin += offset
-    return sin
+    return generate_sin(magnitude=magnitude, noise=noise, offset=offset)
 
-def generate_jumpy_cos(magnitude=10, noise=True, time_count=0, jump_every=50, jump_offset=20):
+
+def generate_jumpy_cos(
+    magnitude=10, noise=True, time_count=0, jump_every=50, jump_offset=20
+):
     time_count = time_count // jump_every
-    if (time_count %  2 == 0):
+    if time_count % 2 == 0:
         offset = jump_offset
     else:
         offset = 0
-
-    cos = np.cos(time.time())
-    if noise:
-        cos += np.random.rand()
-    cos *= magnitude
-    cos += offset
-    return cos
+    return generate_cos(magnitude=magnitude, noise=noise, offset=offset)
