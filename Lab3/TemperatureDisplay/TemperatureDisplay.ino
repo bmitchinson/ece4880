c95a72c98c9acd1eb1da918dc1425850084a2ad3
abpwrs <powersa15@gmail.com>
2019-12-11 22:09:04 -0600

    working on roation

--- Lab3/TemperatureDisplay/TemperatureDisplay.ino
+++ Lab3/TemperatureDisplay/TemperatureDisplay.ino
@@ -3,7 +3,7 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
-#include <stdint.h> 
+#include <stdint.h>
 #include "TouchScreen.h"
 #include <SdFat.h>                // SD card & FAT filesystem library
 #include <Adafruit_SPIFlash.h>    // SPI / QSPI flash library
@@ -13,19 +13,19 @@
 // defs
 #define TFT_DC 9
 #define TFT_CS 10
-#define SD_CS   4 // SD card select pin
+#define SD_CS 4 // SD card select pin
 
 // Touchscreen definitions
-#define YP A2  // must be an analog pin, use "An" notation!
-#define XM A3  // must be an analog pin, use "An" notation!
-#define YM 8   // can be a digital pin
-#define XP 7   // can be a digital pin
+#define YP A2 // must be an analog pin, use "An" notation!
+#define XM A3 // must be an analog pin, use "An" notation!
+#define YM 8  // can be a digital pin
+#define XP 7  // can be a digital pin
 
-// Touch screen variables defining maximum and minum inputs to 
+// Touch screen variables defining maximum and minum inputs to
 // convert between touch screen and display coordinates
 #define TS_MAXX 942
-#define TS_MAXY 890 
-#define TS_MINX 122 
+#define TS_MAXY 890
+#define TS_MINX 122
 #define TS_MINY 111
 
 // view min and max
@@ -33,66 +33,92 @@
 #define VIEW_MAX_Y 320
 
 // initialize global object for touch screen
-# define UNKNOWN_TOUCHSCREEN_PARAM 300
+#define UNKNOWN_TOUCHSCREEN_PARAM 300
 
 // *****************************
 // configure sd card
 #define USE_SD_CARD
 #if defined(USE_SD_CARD)
-  SdFat                SD;         // SD card filesystem
-  Adafruit_ImageReader reader(SD); // Image-reader object, pass in SD filesys
+SdFat SD;                        // SD card filesystem
+Adafruit_ImageReader reader(SD); // Image-reader object, pass in SD filesys
 #else
-  // SPI or QSPI flash filesystem (i.e. CIRCUITPY drive)
-  #if defined(__SAMD51__) || defined(NRF52840_XXAA)
-    Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS,
-      PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
-  #else
-    #if (SPI_INTERFACES_COUNT == 1)
-      Adafruit_FlashTransport_SPI flashTransport(SS, &SPI);
-    #else
-      Adafruit_FlashTransport_SPI flashTransport(SS1, &SPI1);
-    #endif
-  #endif
-  Adafruit_SPIFlash    flash(&flashTransport);
-  FatFileSystem        filesys;
-  Adafruit_ImageReader reader(filesys); // Image-reader, pass in flash filesys
+// SPI or QSPI flash filesystem (i.e. CIRCUITPY drive)
+#if defined(__SAMD51__) || defined(NRF52840_XXAA)
+Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS,
+                                            PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
+#else
+#if (SPI_INTERFACES_COUNT == 1)
+Adafruit_FlashTransport_SPI flashTransport(SS, &SPI);
+#else
+Adafruit_FlashTransport_SPI flashTransport(SS1, &SPI1);
+#endif
+#endif
+Adafruit_SPIFlash flash(&flashTransport);
+FatFileSystem filesys;
+Adafruit_ImageReader reader(filesys); // Image-reader, pass in flash filesys
 #endif
 
 // *****************************
+
+// screen and display objects
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, UNKNOWN_TOUCHSCREEN_PARAM);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
 // structs
-class Component {
+class Component
+{
 public:
-  Component(int mx, int Mx, int my, int My, char* fname, TouchScreen* ts, Adafruit_ILI9341* tft){
+  // constructor
+  Component(int mx, int my, int Mx, int My, char *fname, TouchScreen *ts, Adafruit_ILI9341 *tft)
+  {
+    this->filename = fname;
     this->minX = mx;
     this->maxX = Mx;
     this->minY = my;
     this->maxY = My;
-    this->filename = fname;
     this->tsPtr = ts;
     this->tftPtr = tft;
   }
+  // state
   // position data
   int minX;
   int maxX;
   int minY;
   int maxY;
   // pointers to global variables
-  TouchScreen* tsPtr;
-  Adafruit_ILI9341* tftPtr;
+  TouchScreen *tsPtr;
+  Adafruit_ILI9341 *tftPtr;
   // iamge status
   ImageReturnCode stat;
   // filename to read from
-  char* filename;
-  // draw the Image
-  void render(){
-      Serial.print(F("Loading...\n"));
-      this->stat = reader.drawBMP(this->filename, *(this->tftPtr), this->minX, this->minY);
-      reader.printStatus(this->stat);
+  char *filename;
+
+  // functions
+  void render()
+  {
+    Serial.print(F("Loading..."));
+    Serial.print(this->filename);
+    Serial.print("\nsize:");
+    Serial.print(this->minX);
+    Serial.print(",");
+    Serial.print(this->maxX);
+    Serial.print(",");
+    Serial.print(this->minY);
+    Serial.print(",");
+    Serial.print(this->maxY);
+    Serial.print("\n");
+    
+    Serial.print(F("..DONE"));
+    this->stat = reader.drawBMP(this->filename, *(this->tftPtr), this->minX, this->minY);
+    reader.printStatus(this->stat);
   }
 
-  bool containsPoint(TSPoint p) {
-    if((p.x < this->maxX) and (p.x > this->minX)){
-      if((p.y < this->maxY) and (p.y > this->minY)) {
+  bool containsPoint(TSPoint p)
+  {
+    if ((p.x < this->maxX) and (p.x > this->minX))
+    {
+      if ((p.y < this->maxY) and (p.y > this->minY))
+      {
         return true;
       }
     }
@@ -100,90 +126,218 @@ public:
   }
 };
 
+//class Screen {
+//public:
+//  // constructor
+//  Screen(Component* components, int num_components, TouchScreen* ts, Adafruit_ILI9341* tft) : NUM_COMPONENTS{num_components}{
+//    this->components = componenets;
+//  }
+//  // state
+//  const int NUM_COMPONENTS;
+//  Component* components;
+//};
 
 // *****************************
 
-
 // *****************************
-// screen and display objects
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, UNKNOWN_TOUCHSCREEN_PARAM);
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-ImageReturnCode stat; // Status from image-reading functions
-Adafruit_Image img; // An image loaded into RAM
-// int32_t width  = 0, height = 0;
+// Component demoComponenet  = Component(0, 0, "/demo.bmp", &ts, &tft);
+Component setlock = Component(18, 15,0,0, "/setclock.bmp", &ts, &tft);
+Component setpre = Component(18, 88,0,0, "/setpre.bmp", &ts, &tft);
+Component toghoff = Component(18, 161,80,225, "/toghoff.bmp", &ts, &tft);
+Component toghon = Component(18, 161,80,225, "/toghon.bmp", &ts, &tft);
+Component upbutt = Component(226, 42,0,0, "/upbutt.bmp", &ts, &tft);
+Component downbutt = Component(226, 118,0,0, "/downbutt.bmp", &ts, &tft);
+Component offbar = Component(106, 174,0,0, "/offbar.bmp", &ts, &tft);
+Component autobar = Component(106, 174,0,0, "/autobar.bmp", &ts, &tft);
+Component heatbar = Component(106, 174,0,0, "/heatbar.bmp", &ts, &tft);
+Component acbar = Component(106, 174,0,0, "/acbar.bmp", &ts, &tft);
+Component tempspot = Component(99, 34,0,0, "/tempspot.bmp", &ts, &tft);
 
-Component demoComponenet  = Component(0, VIEW_MAX_X, 0, VIEW_MAX_Y, "/demo.bmp", &ts, &tft);
+
+// state
+enum ControlSetting {
+  OFF,
+  AUTO,
+  HEAT,
+  ON
+};
+
+enum ScreenSetting {
+  MAIN,
+  PRESET_SELECT_SET,
+  PRESET_SET,
+  CLOCK_SET
+};
+
+bool hold = false;
+ControlSetting controlSetting = ControlSetting::OFF;
+ScreenSetting screenSetting = ScreenSetting::MAIN;
+
+void renderHoldButton() {
+  if (hold) {
+    toghon.render();
+  }
+  else {
+    toghoff.render();
+  }
+}
+
+
+void renderControlBar() {
+  switch (controlSetting){
+    case ControlSetting::OFF:
+        offbar.render();
+        break;
+
+    case ControlSetting::AUTO:
+        autobar.render();
+        break;
+
+    case ControlSetting::HEAT:
+        heatbar.render();
+        break;
+
+    case ControlSetting::ON:
+        acbar.render();
+        break;
+  }
+}
+
+void renderMainScreen(){
+  tft.fillScreen(ILI9341_WHITE);
+  setlock.render();
+  setpre.render();
+  renderHoldButton();
+  upbutt.render();
+  downbutt.render();
+  renderControlBar();
+  tempspot.render();
+}
+
+void callbackMainScreen(TSPoint p){
+  Serial.print("callback\n");
+
+  if (toghoff.containsPoint(p)){
+      Serial.print("contained\n");
+
+      hold = !hold;
+      renderHoldButton();
+  }
+}
+
 
 // *****************************
 // program setup
-void setup() {
+void setup()
+{
   // Setup serial
   Serial.begin(9600);
   #if !defined(ESP32)
-    while(!Serial);
+    while (!Serial)
+      ;
   #endif
-  // If the SD is messed up talk about it
-  Serial.print(F("Initializing filesystem..."));
+    // If the SD is messed up talk about it
+    Serial.print(F("Initializing filesystem..."));
   #if defined(USE_SD_CARD)
-    if(!SD.begin(SD_CS, SD_SCK_MHZ(25))) {
+    if (!SD.begin(SD_CS, SD_SCK_MHZ(25)))
+    {
       Serial.println(F("SD begin() failed"));
-      for(;;);
+      for (;;)
+        ;
     }
   #else
-    if(!flash.begin()) {
+    if (!flash.begin())
+    {
       Serial.println(F("flash begin() failed"));
-      for(;;);
+      for (;;)
+        ;
     }
-    if(!filesys.begin(&flash)) {
+    if (!filesys.begin(&flash))
+    {
       Serial.println(F("filesys begin() failed"));
-      for(;;);
+      for (;;)
+        ;
     }
   #endif
+
   // by this point sd is good to go
   Serial.println(F("OK!"));
 
-  tft.begin(); 
+  tft.begin();
   // Fill screen blue. Not a required step, this just shows that we're
   // successfully communicating with the screen.
-  tft.fillScreen(ILI9341_BLUE);
+  tft.fillScreen(ILI9341_WHITE);
   tft.setRotation(1);
-
-  // Serial.print(F("Loading demo.bmp to screen..."));
-  // stat = reader.drawBMP("/demo.bmp", tft, 0, 0);
-  // reader.printStatus(stat);   // How'd we do?
-  demoComponenet.render();
+  renderMainScreen();
 
   delay(1000); // Pause 1 second before moving on to loop()
 }
+
 // *****************************
-// program loop state
-bool run = false;
-// *****************************
+
 // program loop
-void loop(void){
+void loop(void)
+{
   // get and print touch
   TSPoint p = ts.getPoint();
-  if (p.z > ts.pressureThreshhold) {
-     p.x = map(p.x, TS_MAXX, TS_MINX, 320, 0);
-     p.y = map(p.y, TS_MAXY, TS_MINY, 480, 0);
+  if (p.z > ts.pressureThreshhold)
+  {
+    // button press occurred
+    Serial.print("X1 = ");
+    Serial.print(p.x);
+    Serial.print("\tY1 = ");
+    Serial.print(p.y);
+    Serial.print("\t");
+    p.x = map(p.x, TS_MAXX, TS_MINX, 320, 0);
+    p.y = map(p.y, TS_MAXY, TS_MINY, 240, 0);
+//    p.x = 240 - p.x;
+//    int newx = -1 * p.y;
+//    int newy = p.x;
+//    newx += 240;
+//    p.x = newx;
+//    p.y = newy;
+    // p.y=320-p.y;
+    Serial.print("X2 = ");
+    Serial.print(p.x);
+    Serial.print("\tY2 = ");
+    Serial.print(p.y);
+    Serial.print("\tPressure = ");
+    Serial.println(p.z);
+    tft.drawRect(p.x, p.y, 5, 5, ILI9341_BLUE);
 
-     Serial.print("X = "); Serial.print(p.x);
-     Serial.print("\tY = "); Serial.print(p.y);
-     Serial.print("\tPressure = "); Serial.println(p.z);
+    switch(screenSetting){
+      case ScreenSetting::MAIN:
+          Serial.print("main\n");
+          callbackMainScreen(p);
+          //renderMainScreen();
+          break;
+      case ScreenSetting::PRESET_SELECT_SET:
+          break;
+      case ScreenSetting::PRESET_SET:
+          break;
+      case ScreenSetting::CLOCK_SET:
+          break;
+    }
+
+
   }
-  // if(tsPointInRectangle(p, recty)) {
-  //   tft.fillScreen(ILI9341_RED);
-  //   run = true;
-  // } else if (run) {
-  //   tft.fillScreen(ILI9341_WHITE);
-  //   drawRectangle(recty);
-  //   run = false;
-  // }
-  delay(200);
+  drawRectangle(toghoff);
+  delay(1000);
 }
 
 // *****************************
 // helpter function defs
 
-
+void drawRectangle(Component c) {
+    Serial.print("cmin = (");
+    Serial.print(c.minX);
+    Serial.print(",");
+    Serial.print(c.minY);
+    Serial.print(") cmax = ");
+    Serial.print(c.maxX);
+    Serial.print(",");
+    Serial.print(c.maxY);
+    Serial.print(")\n");
+  tft.drawRect(c.minX, c.minY, c.maxX-c.minX, c.maxY-c.minY, ILI9341_RED);
+}
 // *****************************
